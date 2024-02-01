544de02606853dc1c520b1a4fe382a2f9c3339cf
Shay Kelley <shaaayyk@gmail.com>
2024-02-01 02:41:01 -0600

    added logic to handle which move gets clicked and to show its info accordingly

--- src/components/Fight.jsx
+++ src/components/Fight.jsx
@@ -1,25 +1,53 @@
+import { useState } from "react";
 import { useOutletContext } from "react-router-dom";
-import { battle } from "../utilites/battle"
+import { battle } from "../utilites/battle";
 
 export default function Fight() {
+  const [currentMove, setCurrentMove] = useState(null);
+  const [alreadySelected, setAlreadySelected] = useState(false);
   const [partnerPoke, wildPoke] = useOutletContext();
+
+  function handleMoveSelect(move) {
+    setCurrentMove(move);
+    setAlreadySelected(true);
+  }
+
+  function handleSelected(move, partnerPoke, wildPoke) {
+    if (move.name === currentMove.name) {
+      battle(move, partnerPoke, wildPoke);
+      setCurrentMove(null);
+      setAlreadySelected(false);
+    } else {
+      setCurrentMove(move)
+    }
+  }
   return (
-    <div id='fightContainer'>
-      <div id='moveset'>
-        {partnerPoke.moveset.map((move, index) => (
+    <div id="fightContainer">
+      <div id="moveset">
+        {partnerPoke?.moveset.map((move, index) => (
           <p
-          className='move'
-            onClick={() => battle(move, partnerPoke, wildPoke)}
+            className="move"
+            onClick={() =>
+              alreadySelected
+                ? handleSelected(move, partnerPoke, wildPoke)
+                : handleMoveSelect(move)
+            }
             key={index}
-        >
-          {move.name}
-        </p>
+          >
+            {move.name}
+          </p>
         ))}
       </div>
-      <div id='ppBox'>
-        <p>PP</p>
-        <p>Typing</p>
-      </div>
+      {alreadySelected ? (
+        <div id="ppBoxSelected">
+          <p>PP {`${currentMove?.pp}/${currentMove?.pp}`}</p>
+          <p>{currentMove?.type}</p>
+        </div>
+      ) : (
+        <div id="ppBoxUnselected">
+          <p>Select a move for more info!</p>
+        </div>
+      )}
     </div>
-  )
+  );
 }
