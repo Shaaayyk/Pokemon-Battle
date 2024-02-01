5d823befafa0e1639fa6b783335cea550fde37f9
Shay Kelley <shaaayyk@gmail.com>
2024-02-01 03:15:40 -0600

    added edge cases for battle logic

--- src/components/WildPokeLayout.jsx
+++ src/components/WildPokeLayout.jsx
@@ -1,10 +1,13 @@
-export default function WildPokeLayout({wildPoke}) {
+export default function WildPokeLayout({ wildPoke, wildPokeHp }) {
   return (
     <div id="wildPokeLayout">
       <div className="statsContainer">
         <p>{wildPoke?.name}</p>
         <p>Lv{wildPoke?.level}</p>
-        <progress max={wildPoke?.stats.hp} value={wildPoke?.stats.currentHp} />
+        <progress
+          max={wildPoke?.stats.hp}
+          value={wildPokeHp ? wildPokeHp : wildPoke?.stats.currentHp}
+        />
       </div>
       <div className="pokeImage">
         <img
--- src/utilites/battle.js
+++ src/utilites/battle.js
@@ -55,49 +55,53 @@ export function battle(attackingMove, partnerPoke, wildPoke) {
       messages.push("Nothing happened.")
     }
   }
-  // partnerPoke attacking
-  hasAttacked.partnerPoke = true
-  needsToAttack.partnerPoke = false
-  // send a message with move
-  messages.push(`${partnerPoke.name} used ${attackingMove.name}!`)
-  // check to see if the move will do any damage
-  if (attackingMove.power > 0) {
-    // check to see if the move hits
-    const partnerAccCheck = Math.ceil(Math.random() * 100)
-    if (attackingMove.accuracy >= partnerAccCheck) {
-      // wildPoke will take the hit
-      // check to see how much damage
-      const partnerPower = attackingMove.power + partnerPoke.stats.attack
-      const result = partnerPower - wildPoke.stats.defense
-      // check to see if result did any damage
-      if (result > 0) {
-        // check to see if wildPoke dies
-        const wildPokeHealth = wildPoke.stats.currentHp - (result / 2)
-        if (wildPokeHealth <= 0) {
-          // update wildPoke currentHp to 0
-          wildPoke.stats.currentHp = 0
-          needsToAttack.wildPoke = false
-          // send a losing message
-          messages.push(`${wildPoke.name} fainted!`)
+
+  // check to see if partnerPoke still needs to attack
+  if (!hasAttacked.partnerPoke && needsToAttack.partnerPoke && partnerPoke.stats.currentHp > 0) {
+    // partnerPoke attacking
+    hasAttacked.partnerPoke = true
+    needsToAttack.partnerPoke = false
+    // send a message with move
+    messages.push(`${partnerPoke.name} used ${attackingMove.name}!`)
+    // check to see if the move will do any damage
+    if (attackingMove.power > 0) {
+      // check to see if the move hits
+      const partnerAccCheck = Math.ceil(Math.random() * 100)
+      if (attackingMove.accuracy >= partnerAccCheck) {
+        // wildPoke will take the hit
+        // check to see how much damage
+        const partnerPower = attackingMove.power + partnerPoke.stats.attack
+        const result = partnerPower - wildPoke.stats.defense
+        // check to see if result did any damage
+        if (result > 0) {
+          // check to see if wildPoke dies
+          const wildPokeHealth = wildPoke.stats.currentHp - (result / 2)
+          if (wildPokeHealth <= 0) {
+            // update wildPoke currentHp to 0
+            wildPoke.stats.currentHp = 0
+            needsToAttack.wildPoke = false
+            // send a losing message
+            messages.push(`${wildPoke.name} fainted!`)
+          } else {
+            // update partnerPoke currentHp
+            wildPoke.stats.currentHp = wildPoke.stats.currentHp - Math.ceil(result / 2)
+          }
         } else {
-          // update partnerPoke currentHp
-          wildPoke.stats.currentHp = wildPoke.stats.currentHp - Math.ceil(result / 2)
+          // if the result is <= 0 make the damage taken to 1
+          wildPoke.stats.currentHp = wildPoke.stats.currentHp - 1
         }
       } else {
-        // if the result is <= 0 make the damage taken to 1
-        wildPoke.stats.currentHp = wildPoke.stats.currentHp - 1
+        // the move misses
+        // send a miss message
+        messages.push(`${attackingMove.name} missed!`)
       }
     } else {
-      // the move misses
-      // send a miss message
-      messages.push(`${attackingMove.name} missed!`)
+      // send a doesn't do anything message
+      messages.push("Nothing happened.")
     }
-  } else {
-    // send a doesn't do anything message
-    messages.push("Nothing happened.")
   }
   // check to see if wildPoke still needs to attack
-  if (!hasAttacked.wildPoke && needsToAttack.wildPoke) {
+  if (!hasAttacked.wildPoke && needsToAttack.wildPoke && wildPoke.stats.currentHp > 0) {
     hasAttacked.wildPoke = true
     needsToAttack.wildPoke = false
     // randomly select a move from the moveset
